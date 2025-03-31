70b142524dfacfb8b016119a9096945a8495b885
kirchwasser <121969645+kirchwasser@users.noreply.github.com>
2025-03-31 11:24:10 -0500

    Add files via upload

---
+++ arrays1.js
@@ -0,0 +1,8 @@
+let groceryItems = ["Apples", "Bananas", "Yogurt", "Cheese", "Chicken"];
+let priceList = [2, 3, 1];
+
+console.log(groceryItems);
+console.log(priceList);
+
+console.log("Items:", groceryItems[1], groceryItems[4]);
+console.log("Prices:", priceList[0], priceList[1]);
